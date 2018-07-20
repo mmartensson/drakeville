eef1002edd6c9dac20cbe607e93b851d964ef14c
Markus Mårtensson <markus@mayam.com>
2018-07-20 15:34:11 +0200

    Started work on functions.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,68 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const verifyPlayer = (context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  return context.auth;
+};
+
+const childKindForParents = (first, second) => {
+  if (first === second) {
+    throw new functions.https.HttpsError('invalid-argument', 'First and second parent kind may not be the same.');
+  }
+
+  const set = new Set([first,second]);
+
+  if (set.has('eel')) {
+    if (set.has('fire'))
+      return 'ground';
+
+    if (set.has('ground'))
+      return ['clay','green'][Math.round(Math.random())];
+
+    if (set.has('lava'))
+      return 'air';
+  }
+
+  if (set.has('fire')) {
+    if (set.has('ground'))
+    return ['smelt','lava'][Math.round(Math.random())];
+
+    if (set.has('clay'))
+    return 'hard';
+  }
+
+  if (set.has('hard')) {
+    if (set.has('air'))
+      return 'odd';
+    if (set.has('green'))
+      return 'stone';
+  }
+
+  if (set.has('air')) {
+    if (set.has('smelt'))
+      return 'crystal';
+  }
+
+  throw new functions.https.HttpsError('invalid-argument', `Kinds ${first} and ${second} can not mate.`);
+};
+
+exports.mateDragons = functions.https.onCall((data, context) => {
+  const player = verifyPlayer(context);
+
+  const hatches = +new Date() + 1000 * 60 * 2;
+  const kind = childKindForParents(data.first, data.second);
+  const egg = { kind, hatches };
+
+  // Store egg for player
+
+  return { egg };
+});
--- src/actions/dragons.js
+++ src/actions/dragons.js
@@ -6,6 +6,21 @@ Copyright (c) 2018 Markus Mårtensson. All rights reserved.
 export const SPAWN_DRAGON = 'SPAWN_DRAGON';
 export const SLAY_DRAGON = 'SLAY_DRAGON';
 
+export const mateDragons = (first,second) => (dispatch) => {
+  let matingCall = firebase.functions().httpsCallable('mateDragons');
+  matingCall({first,second})
+    .then(result => dispatch(spawnDragon(result.data.kind)))
+    .catch(error => dispatch(reallyNeedErrorHandling(error)))
+};
+
+// FIXME: Something in app, possibly snack bar related 
+const reallyNeedErrorHandling = (error) => {
+  return {
+    type: 'Missing proper error handling',
+    error
+  }
+};
+
 export const spawnDragon = (kind) => {
   return {
     type: SPAWN_DRAGON,
